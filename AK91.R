c7fcbdc81e78de98d42f39f836f29dc8328bb8be
Avenia Ghazarian <avghazarian@gmail.com>
2021-09-21 21:47:10 +0200

    First commit

---
+++ AK91.R
@@ -0,0 +1,91 @@
+library(tidyverse) #loading packages
+library(haven)
+library(dplyr)
+setwd("/Users/aveniaghazarian/Documents/Task4")
+
+Ak91 <- read_dta(file = "NEW7080.dta")
+Ak91_clean <- Ak91%>% rename(
+  AGE	=	v1,
+  AGEQ	=	v2,
+  EDUC	=	v4,
+  ENOCENT	=	v5,
+  ESOCENT	=	v6,
+  LWKLYWGE	=	v9,
+  MARRIED	=	v10,
+  MIDATL	=	v11,
+  MT	=	v12,
+  NEWENG	=	v13,
+  CENSUS	=	v16,
+  QOB	=	v18,
+  RACE	=	v19,
+  SMSA	=	v20,
+  SOATL	=	v21,
+  WNOCENT	=	v24,
+  WSOCENT	=	v25,
+  YOB	=	v27	
+)%>% select(-v8)%>% mutate(COHORT = ifelse(YOB %in% 20:29, 20.29, 
+                                           ifelse(YOB %in% 30:39, 30.39, 
+                                                  ifelse(YOB %in% 40:49, 40.49,20.29))))
+
+Ak91_clean$AGEQ<-ifelse(Ak91_clean$CENSUS==80, Ak91_clean$AGEQ-1900, Ak91_clean$AGEQ)
+Ak91_clean$AGEQSQ<-sqrt(Ak91_clean$AGEQ)
+################## Generating YOB Dummies ###################
+
+Ak91_clean <- Ak91_clean %>% mutate(YR20 = ifelse(YOB==1920|YOB==30|YOB==40, 1, 0),
+                      YR21 = ifelse(YOB==1921|YOB==31|YOB==41, 1, 0),
+                      YR22 = ifelse(YOB==1922|YOB==32|YOB==42, 1, 0),
+                      YR23 = ifelse(YOB==1923|YOB==33|YOB==43, 1, 0),
+                      YR24 = ifelse(YOB==1924|YOB==34|YOB==44, 1, 0),
+                      YR25 = ifelse(YOB==1925|YOB==35|YOB==45, 1, 0),
+                      YR26 = ifelse(YOB==1926|YOB==36|YOB==46, 1, 0),
+                      YR27 = ifelse(YOB==1927|YOB==37|YOB==47, 1, 0),
+                      YR28 = ifelse(YOB==1928|YOB==38|YOB==48, 1, 0),
+                      YR29 = ifelse(YOB==1929|YOB==39|YOB==49, 1, 0),
+                      QTR1 = ifelse(QOB==1, 1, 0),
+                      QTR2 = ifelse(QOB==2, 1, 0),
+                      QTR3 = ifelse(QOB==3, 1, 0),
+                      QTR4 = ifelse(QOB==4, 1, 0),
+                      QTR120 = QTR1*YR20,
+                      QTR121 = QTR1*YR21,
+                      QTR122 = QTR1*YR22,
+                      QTR123 = QTR1*YR23,
+                      QTR124 = QTR1*YR24,
+                      QTR125 = QTR1*YR25,
+                      QTR126 = QTR1*YR26,
+                      QTR127 = QTR1*YR27,
+                      QTR128 = QTR1*YR28,
+                      QTR129 = QTR1*YR29,
+                      QTR220 = QTR2*YR20,
+                      QTR221 = QTR2*YR21,
+                      QTR222 = QTR2*YR22,
+                      QTR223 = QTR2*YR23,
+                      QTR224 = QTR2*YR24,
+                      QTR225 = QTR2*YR25,
+                      QTR226 = QTR2*YR26,
+                      QTR227 = QTR2*YR27,
+                      QTR228 = QTR2*YR28,
+                      QTR229 = QTR2*YR29,
+                      QTR320 = QTR3*YR20,
+                      QTR321 = QTR3*YR21,
+                      QTR322 = QTR3*YR22,
+                      QTR323 = QTR3*YR23,
+                      QTR324 = QTR3*YR24,
+                      QTR325 = QTR3*YR25,
+                      QTR326 = QTR3*YR26,
+                      QTR327 = QTR3*YR27,
+                      QTR328 = QTR3*YR28,
+                      QTR329 = QTR3*YR29)
+
+nrow(Ak91_clean)
+Cohort2030<-Ak91_clean%>%filter(COHORT<20.30)
+save(Cohort2030, file="Cohort2030.dta")
+save(Ak91_clean, file="Ak91_clean.RData")
+
+
+
+
+
+
+
+
+
---
+++ Ak91_analysis.R
@@ -0,0 +1,79 @@
+library(tidyverse) #loading packages
+library(haven)
+library(dplyr)
+setwd("/Users/aveniaghazarian/Documents/Task4")
+
+cohort2030<-load("Cohort2030.RData")
+Ak91_clean<-load("Ak91_clean.RData")
+
+
+########### Regression Formulas ###########
+year<-paste0("YR", 20:28,collapse = "+")
+qtr1<-paste0("QTR", 120:129, collapse = "+")
+qtr2<-paste0("QTR", 220:229, collapse = "+")
+qtr3<-paste0("QTR", 320:329, collapse = "+")
+qtr<-paste(qtr1,qtr2,qtr3, sep="+",collapse = "+")
+
+(fml1 <- as.formula(paste("LWKLYWGE~ EDUC+", paste(year, collapse= "+"))))
+(fml3 <- as.formula(paste("LWKLYWGE~ EDUC+AGEQ+AGEQSQ+", paste(year, collapse= "+"))))
+(fml5 <- as.formula(paste("LWKLYWGE~ EDUC+RACE+MARRIED+SMSA+NEWENG+MIDATL+ENOCENT+WNOCENT+SOATL+ESOCENT+WSOCENT+MT+", paste(year, collapse= "+"))))
+(fml7 <- as.formula(paste("LWKLYWGE~ EDUC+RACE+MARRIED+SMSA+NEWENG+MIDATL+ENOCENT+WNOCENT+SOATL+ESOCENT+WSOCENT+MT+AGEQ+AGEQSQ+", paste(year, collapse= "+"))))
+
+(fml2 <- as.formula(paste("LWKLYWGE~EDUC+", paste0(year, collapse= "+") , "|" , qtr ,"+", paste(year, collapse= "+"))))
+(fml4 <- as.formula(paste("LWKLYWGE~EDUC+AGEQ+AGEQSQ+", paste0(year, collapse= "+") , "|" , qtr ,"+", paste(year, collapse= "+"),"+AGEQ+AGEQSQ")))
+(fml6 <- as.formula(paste("LWKLYWGE~EDUC+RACE+MARRIED+SMSA+NEWENG+MIDATL+ENOCENT+WNOCENT+SOATL+ESOCENT+WSOCENT+MT+", paste0(year, collapse= "+") , "|" , qtr ,"+", paste(year, collapse= "+"), "+RACE+MARRIED+SMSA+NEWENG+MIDATL+ENOCENT+WNOCENT+SOATL+ESOCENT+WSOCENT+MT")))
+(fml8 <- as.formula(paste("LWKLYWGE~EDUC+RACE+MARRIED+SMSA+NEWENG+MIDATL+ENOCENT+WNOCENT+SOATL+ESOCENT+WSOCENT+MT+AGEQ+AGEQSQ+", paste0(year, collapse= "+") , "|" , qtr , "+RACE+MARRIED+SMSA+NEWENG+MIDATL+ENOCENT+WNOCENT+SOATL+ESOCENT+WSOCENT+MT+AGEQ+AGEQSQ+", paste(year, collapse= "+"))))
+
+col1<-lm(fml1, data=Cohort2030)
+col3<-lm(fml3, data=Cohort2030)
+col5<-lm(fml5, data=Cohort2030)
+col7<-lm(fml7, data=Cohort2030)
+
+col2<-ivreg(fml2, data=Cohort2030)
+col4<-ivreg(fml4, data=Cohort2030)
+col6<-ivreg(fml6, data=Cohort2030)
+col8<-ivreg(fml8, data=Cohort2030)
+
+#There is apparently something wrong with col4 and col8 but I couldn't figure out what!
+stargazer(col1, col2, col3, col5, col6, col7, omit=c("NEWENG", "MIDATL", "ENOCENT", "WNOCENT", "SOATL", "ESOCENT", "WSOCENT", "MT", "YR20",
+                                                     "YR21", "YR22", "YR23", "YR24", "YR25", "YR26", "YR27", "YR28")  ,title="Table IV",
+          add.lines = list(c("9 years of birth dummies", "Yes", "YES", "YES", "YES", "YES", "YES"),
+                           c("8 Region of residence dummies", "No","No", "No", "YES", "YES", "YES" )),align= TRUE, no.space = TRUE)
+
+
+################## Figure V #################
+
+time = seq(30.25, 50, 0.25)
+time = as.data.frame(time)
+n=nrow(time)
+data<-Ak91_clean %>%
+  filter(CENSUS == 80 )%>% 
+  select(YOB, QOB, LWKLYWGE) %>%
+  mutate(YQ=YOB + 0.25 * QOB)%>%
+  group_by(YQ) %>% 
+  summarise_all(mean)
+data<-cbind(data, time)
+
+ggplot(data, aes(x = time, y = LWKLYWGE)) + 
+  geom_line() +
+  geom_point(aes(shape = factor(QOB),fill = factor(QOB)), size = 0.3)+
+  xlab("Year of Birth") + 
+  ylab("Log Weekly Earnings")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
